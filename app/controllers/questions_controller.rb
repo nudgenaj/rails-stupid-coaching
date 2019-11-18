d9ee22561264b3aa7dfdadc306590ba34f11691e
Najihah Talib <najihahtalib@gmail.com>
2019-11-18 15:06:46 +0800

    Challenge 2

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+    return ""
+  end
+
+  def answer
+    @your_message = params[:question]
+    if @your_message.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @your_message != "I am going to work right now!"
+      @answer = "I don't care, get dressed and go to work!"
+    else
+      @answer = ""
+    end
+  end
+end
